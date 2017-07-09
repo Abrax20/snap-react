856e19fc6221832ac88051580417093eac3d5e81
Christoph Abs <christoph.abs@nexenio.com>
2017-07-09 14:42:06 +0200

    Add new Components:x

--- src/components/Example.js
+++ src/components/Example.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Container, Header, Content, Footer, Title, Icon } from 'native-base';
+import { Actions } from 'react-native-router-flux';
 
 export default class Example extends Component {
   render() {
@@ -16,6 +17,7 @@ export default class Example extends Component {
         <Footer>
           <Title>
               <Icon
+                onPress={Actions.pageTwo}
                 name="camera"
                 style={{fontSize: 50}}
               />
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,12 +1,27 @@
 // @flow
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
 import Example from './../components/Example.js';
-
+import Camera from './../components/Camera.js';
 export default class AppContainer extends Component {
   render() {
     return (
-      <Example />
+      <Router>
+        <Scene key="root">
+          <Scene key="Example"
+            component={Example}
+            initial={true}
+            hideNavBar
+          />
+          <Scene
+            key="pageTwo"
+            component={Camera}
+            title="Camera"
+            hideNavBar
+           />
+        </Scene>
+      </Router>
     );
   }
 }
